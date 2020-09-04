e08bfa041aa2db95a041f6652d819f1dbf7b8cdd
Hamid Mahmood <hamid.mahmood96@gmail.com>
2020-09-04 16:47:10 +0500

    Updates

---
+++ precache-manifest.402105794421fa7c7d71244b4e604e4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2110f1f0125797a41e87b7564f4c4779",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "1bdd3078e480af68b833",
+    "url": "/react-gh-pages/static/css/main.7f1572ba.chunk.css"
+  },
+  {
+    "revision": "bbfae125f96c44d23201",
+    "url": "/react-gh-pages/static/js/2.d10c7e8f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-gh-pages/static/js/2.d10c7e8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bdd3078e480af68b833",
+    "url": "/react-gh-pages/static/js/main.427c3a69.chunk.js"
+  },
+  {
+    "revision": "2cd7ca98a52ef0f1fa90",
+    "url": "/react-gh-pages/static/js/runtime-main.b157dcd8.js"
+  }
+]);
